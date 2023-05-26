900437aa5f5b8f81e6a326b82516fd9b5a674d35
VitorMendesco <vitormendesco.js@gmail.com>
2023-05-26 12:22:02 -0300

    feat(configs): adding misc project configs

--- src/api/providers/BackendProviders/BackendProviders.tsx
+++ src/api/providers/BackendProviders/BackendProviders.tsx
@@ -4,7 +4,6 @@ import { ReactQueryProvider } from 'api/providers/ReactQueryProvider'
 import { BackendProvidersProps } from './types'
 
 const BackendProviders: FC<BackendProvidersProps> = ({ children }) => (
-  // <SWRProvider>{children}</SWRProvider>
   <ReactQueryProvider>{children}</ReactQueryProvider>
 )
 
