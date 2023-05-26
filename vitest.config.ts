9c626aae6ed6ed65db77c43eafda7e4e7ea2aeb8
VitorMendesco <vitormendesco.js@gmail.com>
2023-05-26 20:21:31 -0300

    feat(vitest): setting up coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,23 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vitest/config'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
+    reporters: ['default'],
+    coverage: {
+      all: true,
+      src: ['src'],
+      exclude: [
+        '__mocks__',
+        '**/*.d.ts',
+        '**/index.ts',
+        '**/*.styles.ts',
+        '**/constants',
+        '**/theme.ts',
+        '**/schema.ts',
+      ],
+    },
     environment: 'jsdom',
     setupFiles: './config/setup.vitest.ts',
   },
