59532afc2e08ab93c59806aca3b87bc03e83b8c4
VitorMendesco <vitormendesco.js@gmail.com>
2023-05-27 13:10:23 -0300

    fix(storybook): removing preview wrappers and customizing background

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,34 +1,34 @@
 import type { Preview } from '@storybook/react'
 
-import { Stack } from '@mui/material'
 import React from 'react'
-import { FrontendContexts } from '../src/contexts/FrontendContexts'
+import { StyleProvider } from '../src/contexts/StyleContext'
 
 const preview: Preview = {
   decorators: [
     Story => (
-      <FrontendContexts>
-        <Stack
-          alignItems="center"
-          bgcolor="#FFF"
-          height="100vh"
-          justifyContent="center"
-          width="100%"
-        >
-          <Story />
-        </Stack>
-      </FrontendContexts>
+      <StyleProvider>
+        <Story />
+      </StyleProvider>
     ),
   ],
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
+    backgrounds: {
+      default: 'light',
+      values: [
+        {
+          name: 'light',
+          value: '#F1F1F1',
+        },
+      ],
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/,
       },
     },
-    layout: 'fullscreen',
+    layout: 'centered',
   },
 }
 
